e092485f535089e132532ace132f2d7659aa9e13
Noor <noorsuboh123n@gmail.com>
2026-04-21 13:55:58 +0300

    Dir scaffolding

---
+++ backend/constants/statusCodes.js
@@ -0,0 +1,14 @@
+// Central place for common HTTP status codes used in the API
+const STATUS_CODE = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+module.exports = STATUS_CODE;
