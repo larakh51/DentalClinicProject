1caadc2f4568126219a0ceba6aec078f350dd32d
Noor <noorsuboh123n@gmail.com>
2026-04-28 14:53:51 +0300

    added appointmentsRoutes.js

---
+++ backend/routes/appointmentsRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAppointments,
+  createAppointment,
+  updateAppointmentStatus,
+} = require("../controllers/appointmentsController");
+
+router.get("/", getAppointments);
+router.post("/", createAppointment);
+router.patch("/:id/status", updateAppointmentStatus);
+
+module.exports = router;
