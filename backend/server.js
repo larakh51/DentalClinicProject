47c177bf93146f43880fd3bb6bb825db9ed89ac6
larakh51 <larakh51@gmail.com>
2026-04-28 14:25:10 +0300

    nothing

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const invoicesRoutes = require("./routes/invoicesRoutes");
 const medicalRecordsRoutes = require("./routes/medicalRecordsRoutes");
 
 const app = express();
-
+//
 app.use(
   cors({
     origin: "http://localhost:5173",
